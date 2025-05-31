d771800cea6472b76d93c39271a0c40072954ba6
vinicius <vinicius_apple22@outloo.com>
2025-05-30 22:33:55 -0300

    começcando a arrumar o front

--- web/web-TrackDrive/routes/web.php
+++ web/web-TrackDrive/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
+use Illuminate\Http\Request;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Exception\Auth\FailedToVerifyToken;
+
 
 Route::get('/', function () {
     return view('home');
@@ -12,5 +17,54 @@ Route::get('/login', function () {
 
 Route::get('/cadastro', function () {
     return view('cadastro');
+});
+
+
+Route::get('/firebase/test-connection', function () {
+    $factory = (new Factory)->withServiceAccount(storage_path('firebase/firebase_credentials.json'));
+    $auth = $factory->createAuth();
+
+    try {
+        $users = $auth->listUsers();
+        $count = iterator_count($users);
+
+        return response()->json([
+            'message' => 'Conexão OK!',
+            'user_count' => $count,
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'message' => 'Erro na conexão',
+            'error' => $e->getMessage(),
+        ], 500);
+    }
+});
+
+Route::post('/firebase/register', function (Request $request) {
+    $factory = (new Factory)->withServiceAccount(storage_path('firebase/firebase_credentials.json'));
+    $auth = $factory->createAuth();
+
+    $data = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required|string|min:6',
+    ]);
+
+    try {
+        $user = $auth->createUser([
+            'email' => $data['email'],
+            'password' => $data['password'],
+        ]);
+
+        return response()->json([
+            'message' => 'Usuário criado com sucesso!',
+            'uid' => $user->uid,
+            'email' => $user->email,
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'message' => 'Erro ao criar usuário',
+            'error' => $e->getMessage(),
+        ], 500);
+    }
 });
     
